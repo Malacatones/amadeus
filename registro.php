580578e6f5967d1e2d7909bdb874925f7dc5e959
Malacatones <>
2013-02-06 21:47:35 +0100

    Miercoles 6 de Febreso

---
+++ registro.php
@@ -0,0 +1,152 @@
+<div class="wrapper">
+
+	       <div class="grids top">
+
+		    <div class="grid-6 grid">
+			 <h2>Dirección</h2>
+			 <div>
+			      <p class="bottom">
+				   Malacatones Jandemor<br />
+				   La 13 Rue del Percebe<br />
+				   Entreplanta 3ª B<br />
+				   Malacatolandia<br /><br />
+				   Telefono: 981 981 981<br />
+				   Fax: 981 189 189<br />
+				   Correo: malacatones@malacatones.com
+                              </p>
+			 </div>
+
+
+			 <div class="green bottom">
+			      <h3>Formalize</h3>
+			      <p>This subpage includes the great JQuery plugin <strong>
+					<a href="http://formalize.me/" title="Formalize Website">Formalize</a></strong>
+				   by Nathan Smith.
+				   <a href="http://formalize.me/" title="Formalize Website">Visit the website</a> to find out what it does and see the demos!
+				   You don´t have to use a table as shown here (this is just an example), you can build your form without tables, too.
+				   Inspect the CSS to utilize the classes that fit your needs.<br />
+				   Comment or delete the section "Forms" in inuit.css when you make use of Formalize so the styles won´t interfere.
+			      </p>
+			 </div>
+
+			 <div>
+			      <p class="message warning bottom">
+				   <b>Nota:</b> Te llenaremos el correo de span, todos los días te intentaremos vender millones de cosas para que nos hagas ricos.</b>
+				   Recuerdalo, nosotros no lo olvidaremos.
+                              </p>
+			 </div>
+		    </div>
+
+
+
+
+		    <!--===============================================================  Contact form =====================================================================================-->
+		    <div class="grid-10 grid">
+
+			 <h2>Unete</h2>
+			 <form  action="#" method="post" enctype="multipart/form-data" onsubmit="return false">
+                              <table class="form">
+				   <tr>
+					<th>
+					     <label for="name">
+						  Nombre
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="text" id="nombre" name="nombre" required="required" placeholder="Ejemplo: María José"/>
+
+					</td>
+				   </tr>
+                                   <tr>
+					<th>
+					     <label for="name">
+						  Apellidos
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="text" id="apellidos" name="apellidos" required="required" placeholder="Ejemplo: García Ramirez" />
+
+					</td>
+				   </tr>
+                                   <tr>
+					<th>
+					     <label for="name">
+						  Nick
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="text" id="nick" name="nick" required="required" placeholder="Ejemplo: Marise" />
+
+					</td>
+				   </tr>
+				   <tr>
+					<th>
+					     <label for="name">
+						  Contraseña
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="password" id="name" name="contraseña" required="required" placeholder="Ejemplo: abc123." />
+
+					</td>
+				   </tr>
+                                   <tr>
+					<th>
+					     <label for="email">
+						  Correo
+					     </label>
+					</th>
+					<td>
+
+					     <input class="input_full" type="email" id="email" name="correo" required="required" placeholder="Ejemplo: marise@correo.com" />
+					</td>
+				   </tr>
+
+				   <tr>
+
+					<th>
+					     <label for="tel">
+						  Telefono
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="tel" id="tel" name="telefono" required="required" placeholder="Ejemplo: 666666666" />
+					</td>
+				   </tr>
+
+				   <tr>
+					<th>
+					     <label for="url">
+						  DNI
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="number" id="url" name="url" required="required" placeholder="Ejemplo: 123456789A" />
+					</td>
+
+				   </tr>
+				   <tr>
+					<th>
+					     <label for="cc">
+						  <abbr title="Courtesy Copy">CC</abbr>
+					     </label>
+
+					</th>
+					<td>
+					     <input type="checkbox" id="cc" name="cc" value="1" />
+					     <label for="cc">
+						  Enviar una copia a mi correo
+					     </label>
+					</td>
+				   </tr>
+                              </table>
+
+                              <p>
+				   <input type="submit" value="Unirse" class="float_left" />
+				   <input type="reset" value="Borrar" class="float_right">
+                              </p>
+			 </form>
+
+
+		    </div><!--end of grid-10-->
+	       </div><!--end of grids-->
